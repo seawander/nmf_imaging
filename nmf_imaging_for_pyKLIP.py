c247e1bb71f2bc034e001a249e81eb31e2a8f457
Bin Ren <renrenbinbin@gmail.com>
2017-06-26 12:21:39 -0400

    Create nmf_imaging_for_pyKLIP.py

---
+++ nmf_imaging_for_pyKLIP.py
@@ -0,0 +1,154 @@
+# This code is the nmf_imaging.py adjusted for pyKLIP, see https://bitbucket.org/pyKLIP/pyklip.
+from NonnegMFPy import nmf
+import numpy as np
+
+def NMFcomponents(ref, ref_err = None, n_components = None, maxiters = 1e3, oneByOne = False):
+    """Returns the NMF components, where the columns contain the information.
+    Input: ref and ref_err should be (N * p) where n is the number of references, p is the number of pixels in each reference.
+    Output: NMf components.
+    """
+    if ref_err is None:
+        ref_err = np.sqrt(ref)
+        
+    if (n_components is None) or (n_components > ref.shape[0]):
+        n_components = ref.shape[0]
+
+    ref[ref < 0] = 0
+    ref_err[ref <= 0] = np.nanpercentile(ref_err, 95)*10 #Setting the err of <= 0 pixels to be max error to reduce their impact
+    
+    ref_columnized = ref.T         #columnize ref, making the columns contain the information
+    ref_err_columnized = ref_err.T # columnize ref_err, making the columns contain the information
+    components_column = 0
+    if not oneByOne:
+        g_img = nmf.NMF(ref_columnized, V=1.0/ref_err_columnized**2, n_components=n_components)
+        chi2, time_used = g_img.SolveNMF(maxiters=maxiters)
+        components_column = g_img.W/np.sqrt(np.nansum(g_img.W**2, axis = 0)) #normalize the components        
+    else:
+        print("Building components one by one...")
+        for i in range(n_components):
+            print("\t" + str(i+1) + " of " + str(n_components))
+            n = i + 1
+            if (i == 0):
+                g_img = nmf.NMF(ref_columnized, V = 1.0/ref_err_columnized**2, n_components= n)
+            else:
+                W_ini = np.random.rand(ref_columnized.shape[0], n)
+                W_ini[:, :(n-1)] = np.copy(g_img.W)
+                W_ini = np.array(W_ini, order = 'F') #Fortran ordering, column elements contiguous in memory.
+                
+                H_ini = np.random.rand(n, ref_columnized.shape[1])
+                H_ini[:(n-1), :] = np.copy(g_img.H)
+                H_ini = np.array(H_ini, order = 'C') #C ordering, row elements contiguous in memory.
+                
+                g_img = nmf.NMF(ref_columnized, V = 1.0/ref_err_columnized**2, W = W_ini, H = H_ini, n_components= n)
+            chi2 = g_img.SolveNMF(maxiters=maxiters)
+            
+            components_column = g_img.W/np.sqrt(np.nansum(g_img.W**2, axis = 0)) #normalize the components
+    
+    return components_column
+    
+def NMFmodelling(trg, components, n_components = None, trg_err = None, maxiters = 1e3, returnChi2 = False, projectionsOnly = False, coefsAlso = False, cube = False, trgThresh = 1.0):
+    """
+    trg: 1D array, p pixels
+    components: p * N, calculated using NMFcomponents.
+    n_components: how many components do you want to use. If None, all the components will be used.
+    
+    projectionsOnly: output the individual projection results.
+    cube: whether output a cube or not (increasing the number of components).
+    trgThresh: ignore the regions with low photon counts. Especially when they are ~10^-15 or smaller. I chose 1 in this case.
+    """
+    if n_components is None:
+        n_components = components.shape[1]
+        
+    if trg_err is None:
+        trg_err = np.sqrt(trg)
+        
+    trg[trg < trgThresh] = 0
+    trg_err[trg == 0] = np.nanpercentile(trg_err, 95)*10
+    
+    components_column = components
+    components_column = components_column/np.sqrt(np.nansum(components_column**2, axis = 0)) #normalize the components #make sure the components are normalized.
+    
+    trg_column = np.zeros((trg.shape[0], 1))
+    trg_column[:, 0] = trg
+    trg_err_column = np.zeros((trg_err.shape[0], 1))
+    trg_err_column[:, 0] = trg_err
+    if not cube:
+        trg_img = nmf.NMF(trg_column, V=1/trg_err_column**2, W=components_column, n_components = n_components)
+        (chi2, time_used) = trg_img.SolveNMF(H_only=True, maxiters = maxiters)
+    
+        coefs = trg_img.H
+        
+        if not projectionsOnly:
+            # return only the final result
+            model_column = np.dot(components_column, coefs)
+    
+        else:
+            # return the individual projections
+            if not coefsAlso:
+                return (coefs.flatten() * components.T).T
+            else:
+                return (coefs.flatten() * components.T).T, coefs
+    else:
+        print("Building models one by one...")
+        
+        for i in range(n_components):
+            print("\t" + str(i+1) + " of " + str(n_components))
+            trg_img = nmf.NMF(trg_column, V=1/trg_err_column**2, W=components_column[:, :i+1], n_components = i + 1)
+            (chi2, time_used) = trg_img.SolveNMF(H_only=True, maxiters = maxiters)
+    
+            coefs = trg_img.H
+    
+            model_column = np.dot(components_column[:, :i+1], coefs)
+            
+    if returnChi2:
+        return model_column, chi2
+    if coefsAlso:
+        return model_column, coefs
+    return model_column
+    
+def NMFsubtraction(trg, model, frac = 1):
+    """NMF subtraction with a correction factor, frac."""
+    if np.shape(np.asarray(frac)) == ():
+        return trg-model*frac
+    result = np.zeros((len(frac), ) + model.shape)
+    for i, fraction in enumerate(frac):
+        result[i] = trg-model*fraction
+    return result
+    
+def NMFbff(trg, model, fracs = None):
+    """BFF subtraction.
+    Input: trg, model, fracs (if need to be).
+    Output: best frac
+    """
+    
+    if fracs is None:
+        fracs = np.arange(0.60, 1.001, 0.001)
+    
+    std_infos = np.zeros(fracs.shape)
+    
+    for i, frac in enumerate(fracs):
+        data_slice = trg - model*frac
+        while 1:
+            if np.nansum(data_slice > np.nanmedian(data_slice) + 3*np.nanstd(data_slice)) == 0 or np.nansum(data_slice < np.nanmedian(data_slice) -10*np.nanstd(data_slice)) == 0:
+                break
+            data_slice[data_slice > np.nanmedian(data_slice) + 3*np.nanstd(data_slice)] = np.nan
+            data_slice[data_slice < np.nanmedian(data_slice) - 10*np.nanstd(data_slice)] = np.nan
+        std_info = np.nanstd(data_slice)
+        std_infos[i] = std_info
+    return fracs[np.where(std_infos == np.nanmin(std_infos))]   
+   
+def nmf_function(trg, refs, trg_err = None, refs_err = None, mask = None, componentNum = 5, maxiters = 1e5, oneByOne = True):
+    """ Main NMF function for high contrast imaging.
+    Input:  trg (2D array): target image, dimension: height * width.
+            refs (3D array): reference cube, dimension: referenceNumber * height * width.
+            trg_err, ref_err: uncertainty for trg and refs, repectively. If None is given, the squareroot of the two arrays will be adopted.
+            mask (2D array): 0 and 1 array, the mask of the region we are interested in for NMF. 1 means the pixel we are interested in.
+            componentNum (integer): number of components to be used. Default: 5. Caution: choosing too many components will slow down the computation.
+            maxiters (integer): number of iterations needed. Default: 10^5.
+            oneByOne (boolean): whether to construct the NMF components one by one. Default: True.
+    Output: result (2D array): NMF modeling result. Only the final subtraction result is returned."""
+    components = NMFcomponents(refs, ref_err = refs_err, mask = mask, n_components = componentNum, maxiters = maxiters, oneByOne=oneByOne)
+    model = NMFmodelling(trg = trg, components = components, n_components = componentNum, trg_err = trg_err, mask_components=mask, maxiters=maxiters)
+    best_frac = NMFbff(trg = trg, model = model, mask = mask)
+    result = NMFsubtraction(trg = trg, model = model, mask = mask, frac = best_frac)
+    return result
