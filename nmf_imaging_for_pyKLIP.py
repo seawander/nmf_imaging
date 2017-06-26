90fc749dac046ae73911bf0a082e292a4c2cd9e0
Bin Ren <renrenbinbin@gmail.com>
2017-06-26 12:49:30 -0400

    Update nmf_imaging_for_pyKLIP.py

--- nmf_imaging_for_pyKLIP.py
+++ nmf_imaging_for_pyKLIP.py
@@ -142,7 +142,7 @@ def NMFbff(trg, model, fracs = None):
         std_infos[i] = std_info
     return fracs[np.where(std_infos == np.nanmin(std_infos))]   
    
-def nmf_function(trg, refs, trg_err = None, refs_err = None, mask = None, componentNum = 5, maxiters = 1e5, oneByOne = True):
+def nmf_math(trg, refs, trg_err = None, refs_err = None, mask = None, componentNum = 5, maxiters = 1e5, oneByOne = True):
     """ Main NMF function for high contrast imaging.
     Input:  trg (1D array): target image, dimension: height * width.
             refs (2D array): reference cube, dimension: referenceNumber * height * width.
